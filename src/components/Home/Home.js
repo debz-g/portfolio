ad9aaa8a3623ca5d318a1833049c566118dcf6c6
debz-g <debz.exe@gmail.com>
2023-05-16 21:29:27 +0530

    Changes name

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> DEBAYAN GHOSH</strong>
+                <strong className="main-name"> DEBAYAN GHOSH DASTIDER</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
